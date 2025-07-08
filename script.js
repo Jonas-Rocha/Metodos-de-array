06d1c7d62b0f4beff8e9f3e49e4e68aa0a561e0c
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-07-08 20:33:17 -0300

    finalizando o metodo some()

--- script.js
+++ script.js
@@ -103,3 +103,14 @@ const ages = [15, 30, 39, 29];
 // Verificando se todas as idades são maiores ou igual a 18.
 const result2 = ages.every((age) => age >= 18);
 console.log(result2);
+
+/*
+O método some() testa se ao menos um dos elementos no array
+passa na condição e retorna um valor true ou false.
+*/
+
+// Exemplo de array de idades
+const ages1 = [15, 30, 39, 29];
+
+const result3 = ages.some((age) => age < 18);
+console.log(result3);
