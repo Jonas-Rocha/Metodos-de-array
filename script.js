7b50a2303690dfbd4321f46972e03df710bdeeb1
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-07-08 13:11:04 -0300

    finalizando o metodo filter()

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ const formated = products.map((item) => {
 });
 
 console.log(formated);
+
+/*
+O método filter() cria um novo array com todos os elementos que passaram na condição.
+*/
+
+const words = ["Javascript", "HTML", "CSS", "Web"];
+
+const result = words.filter((word) => word.length > 3);
+console.log(result);
+
+const products1 = [
+  { description: "Teclado", price: 150, promotion: true },
+  { description: "Mouse", price: 70, promotion: false },
+  { description: "Monitor", price: 1200, promotion: true },
+];
+
+// Exemplo de um filtro de produtos na promoção.
+const promotion = products1.filter((item) => item.promotion === true);
+/*
+O filter é bom para quando queremos percorrer um array ou objeto
+e retornar com um novo array filtrado.
+*/
+
+console.log(promotion);
